2afacd5dcb9a3a8c2a4ef2cd5cb88b9be07c1e3a
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-12-10 17:18:21 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const input = document.querySelector('input')
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
+});
